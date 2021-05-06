c881b8b57f0ef8d1f9acb30a7ff190cf1abf8ab0
SRNV <alula.rudy@gmail.com>
2021-05-06 09:57:02 +0200

    revert test

--- tests/component_test.ts
+++ tests/component_test.ts
@@ -6,7 +6,6 @@ import {
 import { component1 } from "./utils/componentFile.ts";
 
 const url = new URL(import.meta.url);
-/*
 // TODO
 Deno.test("ogone-lexer can parse a basic component", () => {
   const style = `
@@ -43,7 +42,6 @@ import component A from './b.o3';
     throw new Error("Exium - Failed to retrieve Node Context");
   }
 });
-*/
 
 Deno.test("ogone-lexer large component is parsed < 100ms", () => {
   const perf = performance.now();
